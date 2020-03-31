ffbcb4150eeae568a916cd73193b79416529b632
Anna <18068391@student.hhs.nl>
2020-03-31 20:52:33 +0200

    Javascript API, CSS

--- challenge3.js
+++ challenge3.js
@@ -1,25 +1,53 @@
-// In deze funcie ga ik API data op de website tonen, in dit geval de teperatuur in Den Haag op dit moment
-function getData() {
-	//Link naar de API datata plek. De soort info, de stad en je account.
-	var url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-	var city = 'the%20Hague';
-	var apiKey ='71651970a1f747f6f025a4c39785894b';
-	//Het is eigelijk een hele link maar de link is in stukjes gehakt zodat je makkelijke delen kan veranderen bijvoorbeeld de stad.
-	var API = url + city + '&appid=' + apiKey;
+// lat en long to string van function to global variabele zodat gebruik bij api
 
-	//Nu hebben we API data die javascript niet begrijpt, we gaan het dus omvormen naar .json data
+var latitude= '36.778259';
+var longitude= '-119.417931';
+
+function infoLandingPad(){
+	var API='https://api.spacexdata.com/v3/landpads/LZ-4';
 	fetch(API)
 	.then(function(data) {
 		return data.json();
 	})
-	//Nu kunnen we werken met de data
 	.then(function(data) {
-		//hier reken ik bijvoorbeeld kelvin om naar celcius
-		var tempperatuur = Math.round(data.main.temp - 273.15);
-		//hier stop ik het in de HTML
-		document.getElementById('weerbox').innerHTML = tempperatuur+'&#176;C';
+		//info
+		var naam =data.location.name+" - "+data.full_name;
+		var regio=data.location.region;
+		document.getElementById('naam').innerHTML = naam;
+		document.getElementById('regio').innerHTML = regio;
+		//locatie
+		latitude = data.location.latitude;
+		longitude = data.location.longitude;
+	});
+}
+
+function infoWeer() {
+	var url = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+	var apiKey ='71651970a1f747f6f025a4c39785894b';
+	var API = url + latitude +'&lon='+ longitude + '&appid=' + apiKey+'&units=metric'+'&lang=nl';
+	fetch(API)
+	.then(function(data) {
+		return data.json();
+	})
+	.then(function(data) {
+		var temperatuur=Math.round(data.main.temp);
+		var wind=data.wind.speed;
+		var zicht=data.visibility;
+		document.getElementById('temperatuur').innerHTML = temperatuur+'&#176;C';
+		document.getElementById('wind').innerHTML = wind+' meter/sec';
+		document.getElementById('zicht').innerHTML = zicht+'m ver zicht';
+		if(temperatuur>=5 && temperatuur<=25){
+			document.getElementById('temperatuur').classList.add("check");
+		}
+		if(wind<=3.3){
+			document.getElementById('wind').classList.add("check");
+		}
+		if(zicht>=19000){
+			document.getElementById('zicht').classList.add("check");
+		}
 	});
 }
 
 // alle functies activeren
-getData();
+infoLandingPad();
+infoWeer();
